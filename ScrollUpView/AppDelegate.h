ba8e546c21bc78ab28e662aa6728c922c9e10714
李潇 <lixiao@lxmacbook.lan>
2016-03-14 15:25:09 +0800

    Initial Commit

---
+++ ScrollUpView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollUpView
+//
+//  Created by 李潇 on 16/3/14.
+//  Copyright © 2016年 boai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollUpView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollUpView
+//
+//  Created by 李潇 on 16/3/14.
+//  Copyright © 2016年 boai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
